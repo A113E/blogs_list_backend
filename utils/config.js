7afe6463512247131fa79a916b54fd0a2f8f5e74
A113E <amg11amg2@gmail.com>
2025-01-10 13:57:47 -0500

    Rutas definidas backend

---
+++ utils/config.js
@@ -0,0 +1,9 @@
+// Importar el archivo .env del dotenv *IMPORTANTE PONER PRIMERO QUE EL MODULO NOTE*
+require('dotenv').config()
+const PORT = process.env.PORT
+const MONGODB_URI = process.env.MONGODB_URI
+// Exportar modulo
+module.exports = {
+  MONGODB_URI,
+  PORT
+}
---
+++ utils/logger.js
@@ -0,0 +1,28 @@
+// Módulo para las las impresiones a la consola
+// Creamos una instancia para todos los tipos de impresiones
+const logger = {
+  // Este objeto contiene varias funciones (info, debug, warn, error) que representan diferentes niveles de logs.
+
+  // Función INFO
+  info: (message) => {
+    console.log(`[INFO] ${message}`); // Salida: [INFO] Server running on port 3001
+  },
+
+  // Función DEBUG
+  debug: (message) => {
+    console.log(`[DEBUG] ${message}`); // Salida: [DEBUG] Database connection string: mongodb://localhost/test
+  },
+
+  // Función WARN
+  warn: (message) => {
+    console.log(`[WARN] ${message}`); // Salida: [WARN] Deprecated API endpoint accessed.
+  },
+
+  // Función ERROR
+  error: (message) => {
+    console.log(`[ERROR] ${message}`); // Salida: [ERROR] Failed to connect to the database.
+  },
+};
+
+// Exportar el módulo
+module.exports = logger;
